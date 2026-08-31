39d0c653c14778ec1fc51a484f727c79f7753da8
tamsir <tamsir.anne@hotmail.de>
2026-08-31 17:18:55 +0000

    Initial commit with Learning Project

---
+++ src/FirstJavaClass.java
@@ -0,0 +1,12 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
