ad2fc7120a355c85db9ea383c71875e53b238e8c
tamsir <tamsir.anne@hotmail.de>
2026-09-02 18:41:29 +0000

    changes FirstJavaClass

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,12 +1,21 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class FirstJavaClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		@Test
+	    public void testAddition() {
+	        assertEquals(4, 2 + 2);
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println("Hello from Java!");
+	    }
 	}
 
-}
+
