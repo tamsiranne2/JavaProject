1f52c9a6d242f97a1160c7d729f088231298f62b
tamsir <tamsir.anne@hotmail.de>
2026-09-05 17:41:20 +0000

    class name changed

--- src/test/java/FirstJavaClassTest.java
+++ src/test/java/FirstJavaClassTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class FirstJavaClass {
+public class FirstJavaClassTest {
 
 
 		@Test
